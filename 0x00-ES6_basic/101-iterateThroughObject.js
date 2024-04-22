3031bdcf01865c64fb5d4f20d1f6a7d816c54c86
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-04-22 21:28:38 +0000

    task 14 fixed

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
   let result = '';
   for (const employee of reportWithIterator) {
-    result += employee + ' | ';
+    result += `${employee} | `;
   }
   // Remove the last ' | ' from the result
   return result.slice(0, -3);
