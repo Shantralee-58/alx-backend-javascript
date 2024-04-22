e44463293a85d561d81663eb7fe64933eea54131
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-04-22 21:25:55 +0000

    task 13 fixed

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,19 +1,9 @@
 export default function createIteratorObject(report) {
   let employees = [];
-  for (let department in report.allEmployees) {
-    employees = [...employees, ...report.allEmployees[department]];
+  for (const department in report.allEmployees) {
+    if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+      employees = employees.concat(report.allEmployees[department]);
+    }
   }
-
-  let currentIndex = 0;
-  return {
-    next() {
-      return {
-        value: employees[currentIndex++],
-        done: currentIndex > employees.length,
-      };
-    },
-    [Symbol.iterator]() {
-      return this;
-    },
-  };
+  return employees[Symbol.iterator]();
 }
